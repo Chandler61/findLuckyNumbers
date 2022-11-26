4e6b128ea7ebd0742a6ce0a425bfff459c9ca388
Chandler Mitchell <sunshined74@yahoo.com>
2022-11-26 16:10:35 -0700

    final commit

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,4 +1,11 @@
 // Write your code below this line.
 function uniqueNums(n){
-    
+    let randomNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    let luckyNumbers = []
+    for (let i = 0; i <= n; i++) {
+        let p = Math.floor(Math.random() * randomNums.length);
+        luckyNumbers.push(randomNums[p])
+        randomNums.slice(p, 1)
+    }
+    return luckyNumbers
 }
